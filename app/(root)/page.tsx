05fcca0af22f906402ce9a1cec1eb19bc086e25f
fabio-martin <fabiomartin2006@gmail.com>
2024-06-26 15:21:41 +0100

    Implement the complete layout with the sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 import React from "react";
 
 export default function Home() {
-  const loggedIn = { firstName: "John" };
+  const loggedIn = {
+    firstName: "John",
+    lastName: "Doe",
+    email: "contact@johndoe.com",
+  };
 
   return (
     <section className="home">
@@ -22,7 +27,13 @@ export default function Home() {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
     </section>
   );
 }
---
+++ app/(root)/payment-transfer/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Transfer = (props: Props) => {
+  return <div>Transfer</div>;
+};
+
+export default Transfer;
